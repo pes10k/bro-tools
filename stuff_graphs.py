1ddecb2c8a12eee7afdaa3b53b55777cc2ac585b
Peter Snyder <snyderp@gmail.com>
2014-06-04 18:49:43 -0500

    -

--- stuff_graphs.py
+++ stuff_graphs.py
@@ -7,21 +7,14 @@ import brotools.records
 import stuffing.amazon
 
 parser = brotools.reports.default_cli_parser(sys.modules[__name__].__doc__)
-parser.add_argument('--graphs', '-g', default="/tmp/bro-redirectors",
-                    help="A path to write images of graphs that include " +
-                    "cookie stuffing attempts to.")
 ins, out, debug, args = brotools.reports.parse_default_cli_args(parser)
-
-brotools.records.BroRecord.__str__ = lambda x: x.host + "\n" + x.uri
 
 for path, graphs in ins:
     debug("Considering {0}".format(path))
     debug("Found {0} graphs".format(len(graphs)))
-    index = 0
     for g in graphs:
-        if "amazon.com" not in g._root.host:
+        if "amazon.com" in g._root.host:
             continue
-        index += 1
         # Iterate over all nodes in the graph until we hit one that has
         # a cookie stuffing attempt in it.  But no need to iterate further
         # after we hit the first stuffing node
