f280359ef80459ac9f172e0faae5fee90431a325
Peter Snyder <snyderp@gmail.com>
2014-07-01 11:33:26 -0500

    nicer reports

--- ip_ua_reuse.py
+++ ip_ua_reuse.py
@@ -55,14 +55,30 @@ num_reused_ips = sum([1 if len(v) > 1 else 0 for v in ip_tokens.values()])
 out.write("IP Aliasing\n")
 out.write("# IPs: {0}\n".format(len(ip_tokens)))
 out.write("Reused IPS: {0}\n".format(num_reused_ips))
-out.write("----------")
-pprint.pprint(ip_tokens, stream=out)
+out.write("----------\n\n")
+for ip, tokens in ip_tokens.items():
+    out.write("IP: {0}\n".format(ip))
+    for t, dates in tokens.items():
+        out.write(" * Session Token: {0}\n".format(t))
+        for d in dates:
+            out.write(" * * {0}\n".format(d))
+    out.write("\n")
 out.write("\n\n")
 
 num_reused_ip_ua = sum([1 if len(v) > 1 else 0 for v in ip_ua_tokens.values()])
 out.write("IP/UA Aliasing\n")
 out.write("# IPs / UA Pairs: {0}\n".format(len(ip_ua_tokens)))
 out.write("Reused IPS / UA Pairs: {0}\n".format(num_reused_ip_ua))
-out.write("----------")
-pprint.pprint(ip_ua_tokens, stream=out)
+out.write("----------\n")
+for key, tokens in ip_tokens.items():
+    parts = key.split(" ")
+    ip = parts[0]
+    ua = " ".join(parts[1:])
+    out.write("IP: {0}\n".format(ip))
+    out.write("UA: {0}\n".format(ua))
+    for t, dates in tokens.items():
+        out.write(" * Session Token: {0}\n".format(t))
+        for d in dates:
+            out.write(" * * {0}\n".format(d))
+    out.write("\n")
 out.write("\n\n")
