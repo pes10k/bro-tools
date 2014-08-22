4590a62a36a8c1be1d844b6812047c2386c820e2
Peter Snyder <snyderp@gmail.com>
2014-08-21 17:02:17 -0700

    -

---
+++ graph_count.py
@@ -0,0 +1,24 @@
+"""Count the number of graphs in one or more files."""
+
+import sys
+import brotools.reports
+import brotools.records
+
+parser = brotools.reports.default_cli_parser(sys.modules[__name__].__doc__)
+count, ins, out, debug, args = brotools.reports.parse_default_cli_args(parser)
+
+debug("Preparing to reading {0} sets of graphs".format(count))
+
+last_path = None
+current_count = 0
+total_count = 0
+for graph, path in ins():
+
+    if last_path and last_path != path:
+        out.write("{0}: {1}\n".format(last_path, current_count))
+
+    last_path = path
+    current_count += 1
+    total_count += 1
+
+out.write("Total: {0}\n".format(total_count))
