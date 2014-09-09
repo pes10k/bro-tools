190a9c524a311d273575cb02864f1747a0cac969
Peter Snyder <snyderp@gmail.com>
2014-09-08 19:00:39 -0500

    shameful rewrite of stats code

--- graph_stats.py
+++ graph_stats.py
@@ -11,12 +11,18 @@ from stuffing.amazon import AmazonAffiliateHistory
 
 
 parser = brotools.reports.default_cli_parser(sys.modules[__name__].__doc__)
-parser.add_argument('--graphs', '-g', action="store_true",
-                    help="If provided, a list of graphs will be included in " +
-                    "the report, describing cookie stuffs, etc.")
-count, ins, out, debug, args = brotools.reports.parse_default_cli_args(parser)
+parser.add_argument('--ttl', type=int, default=84600,
+                    help="The time, in seconds, that an Amazon set affiliate " +
+                    "marketing cookie is expected to be valid.  Default is " +
+                    "one day (84600 seconds)")
+parser.add_argument('--secs', type=int, default=3600,
+                    help="The minimum time in seconds that must pass between " +
+                    "a client's requests to the marketers 'add to cart' page " +
+                    "for those requests to be treated as a seperate checkout")
+count, ins, out, debug, marketers, args = brotools.reports.parse_marketing_cli_args(parser)
 
 debug("Preparing to reading {0} sets of graphs".format(count))
+
 browser_attributes = (
     'is_mobile',
     'is_tablet',
@@ -41,47 +47,31 @@ num_amazon_requests = 0
 num_amazon_roots = 0
 num_amazon_no_referrer = 0
 
-if args.graphs:
-    amazon_stuff_graphs = []
-    amazon_checkout_graphs = []
-    amazon_set_graphs = []
-    amazon_action_graphs = []
-    amazon_high_action_graphs = []
-else:
-    num_amazon_stuffs = 0
-    num_amazon_checkouts = 0
-    num_amazon_sets = 0
+amazon_stuff_graphs = []
+amazon_checkout_graphs = []
+amazon_set_graphs = []
+amazon_missing_sess_id = 0
 
 hosts = {}
 invalid_referrers = []
 index = 0
+
+# Multi indexed dict, in the following format:
+#
+# "Marketer name 1": {
+#    "client 1 hash": history object,
+#    "client 2 hash": history object
+# },
+# "Marketer name 2": {
+#    "client_hash": history object
+# },
+history_by_client = {}
+
 for path, graph in ins():
     index += 1
     num_graphs += 1
     debug("{0}-{1}. Considering {2}".format(index, count, path))
     num_requests += len(graph)
-    num_amazon_requests += sum([len(graph.nodes_for_host(h)) for h in amazon_ish_hosts])
-
-    if args.graphs:
-        local_amazon_stuff_graphs = AmazonAffiliateHistory.stuffs_in_graph(graph)
-        local_amazon_checkout_graphs = AmazonAffiliateHistory.checkouts_in_graph(graph)
-        local_amazon_set_graphs = AmazonAffiliateHistory.cookie_sets_in_graph(graph)
-
-        local_amazon_action_graphs = (set(local_amazon_stuff_graphs)
-            .intersection(set(local_amazon_checkout_graphs)))
-
-        local_amazon_high_action_graphs = (local_amazon_action_graphs
-            .intersection(set(local_amazon_set_graphs)))
-
-        amazon_stuff_graphs += local_amazon_stuff_graphs
-        amazon_checkout_graphs += local_amazon_checkout_graphs
-        amazon_set_graphs += local_amazon_set_graphs
-        amazon_action_graphs += local_amazon_action_graphs
-        amazon_high_action_graphs += local_amazon_high_action_graphs
-    else:
-        num_amazon_stuffs += len(AmazonAffiliateHistory.stuffs_in_graph(graph))
-        num_amazon_checkouts += len(AmazonAffiliateHistory.checkouts_in_graph(graph))
-        num_amazon_sets += len(AmazonAffiliateHistory.cookie_sets_in_graph(graph))
 
     head_record = graph._root
 
@@ -117,6 +107,49 @@ for path, graph in ins():
     except KeyError:
         hosts[referrer_host] = 1
 
+    num_amazon_requests += sum([len(graph.nodes_for_host(h)) for h in amazon_ish_hosts])
+
+    local_amazon_stuff_graphs = AmazonAffiliateHistory.stuffs_in_graph(graph)
+    local_amazon_checkout_graphs = AmazonAffiliateHistory.checkouts_in_graph(graph)
+    local_amazon_set_graphs = AmazonAffiliateHistory.cookie_sets_in_graph(graph)
+
+    amazon_stuff_graphs += local_amazon_stuff_graphs
+    amazon_checkout_graphs += local_amazon_checkout_graphs
+    amazon_set_graphs += local_amazon_set_graphs
+
+    # Since we already have to check to see if there are any match graphs,
+    # we can save some double duty, and only try and slot new graphs into
+    # a purchase history if there is at least one already found interesting
+    # graph
+    interesting_graphs = amazon_stuff_graphs + amazon_checkout_graphs + amazon_set_graphs
+    if len(interesting_graphs) == 0:
+        continue
+
+    # First extract the dict for this AmazonAffiliateHistory
+    try:
+        client_dict = history_by_client[AmazonAffiliateHistory.name()]
+    except:
+        client_dict = {}
+        history_by_client[AmazonAffiliateHistory.name()] = client_dict
+
+    for g in interesting_graphs:
+
+        # See if we can find a session tracking cookie for this visitor
+        # in this graph.  If not, then we know there are no cookie stuffs,
+        # checkouts, or other relevant activity in the graph we
+        # care about, so we can continue
+        hash_key = AmazonAffiliateHistory.session_id_for_graph(g)
+        if not hash_key:
+            amazon_missing_sess_id += 1
+            continue
+
+        # Next, try to extract a history object for this client
+        # out of the dict of clients for the given AmazonAffiliateHistory
+        try:
+            client_dict[hash_key].consider(g)
+        except KeyError:
+            client_dict[hash_key] = AmazonAffiliateHistory(g)
+
 out.write("General Stats\n")
 out.write("===\n")
 out.write("# requests:              {0:10}\n".format(num_requests))
@@ -133,33 +166,40 @@ out.write("===\n")
 out.write("# requests to Amazon:    {0:10}\n".format(num_amazon_requests))
 out.write("# Amazon graph roots:    {0:10}\n".format(num_amazon_roots))
 out.write("# requests w/o referrer: {0:10}\n".format(num_amazon_no_referrer))
-
-if args.graphs:
-    out.write("# Checkouts:      {0:10}\n".format(len(amazon_checkout_graphs)))
-    out.write("# Sets:           {0:10}\n".format(len(amazon_stuff_graphs)))
-    out.write("# Stuffs:         {0:10}\n".format(len(amazon_set_graphs)))
-    out.write("# CO + Stuff:     {0:10}\n".format(len(amazon_action_graphs)))
-    out.write("# CO + SET + STF: {0:10}\n".format(len(amazon_high_action_graphs)))
-else:
-    out.write("# Amazon checkouts:      {0:10}\n".format(num_amazon_checkouts))
-    out.write("# Amazon sets:           {0:10}\n".format(num_amazon_sets))
-    out.write("# Amazon stuffs:         {0:10}\n".format(num_amazon_stuffs))
+out.write("# Checkouts:             {0:10}\n".format(len(amazon_checkout_graphs)))
+out.write("# Sets:                  {0:10}\n".format(len(amazon_stuff_graphs)))
+out.write("# Stuffs:                {0:10}\n".format(len(amazon_set_graphs)))
 out.write("\n")
 
-if args.graphs:
-    reports = (
-        ("Sets, Stuffs & Checkouts", amazon_high_action_graphs),
-        ("Stuffs & Checkouts", amazon_action_graphs),
-        ("Checkouts", amazon_checkout_graphs),
-        ("Sets", amazon_set_graphs),
-        ("Stuffs", amazon_stuff_graphs)
-    )
-    for label, graphs in reports:
-        out.write("{0}\n---\n".format(label))
-        for g in graphs:
-            out.write(str(g))
+out.write("Stuff / Set / Checkout Stats\n")
+out.write("===\n")
+for marketer_name, histories in history_by_client.items():
+    for client_hash, h in histories.items():
+        for c in h.checkouts(seconds=args.secs, cookie_ttl=args.ttl):
+            num_stuffs, num_sets, num_carts = c.counts()
+            # Only print out graph information here if there is at least
+            # two items of interest in the graph (ie a stuff and a set, or
+            # something similar)
+            if num_stuffs == 0 and num_sets == 0:
+                continue
+
+            out.write(str(c))
             out.write("\n\n")
+out.write("\n")
+
+out.write("Checkout / set / stuff graph Stats\n")
+out.write("===\n")
+reports = (
+    ("Checkouts", amazon_checkout_graphs),
+    ("Sets", amazon_set_graphs),
+    ("Stuffs", amazon_stuff_graphs)
+)
+for label, graphs in reports:
+    out.write("{0}\n---\n".format(label))
+    for g in graphs:
+        out.write(str(g))
         out.write("\n\n")
+    out.write("\n\n")
 
 out.write("Invalid Looking Referrers\n")
 out.write("===\n")
