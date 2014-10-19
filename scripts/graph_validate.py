028a15ec96ec60209fd1f2093764638d72dfef84
Peter Snyder <snyderp@gmail.com>
2014-10-18 23:16:56 -0500

    add gzip support to the 'how we doing' script

--- scripts/graph_validate.py
+++ scripts/graph_validate.py
@@ -1,6 +1,6 @@
-#/usr/bin/env python
-"""Builds graphs from the lines in a bro log in a temporary location, echos
-out the number of records incorporated into graphs in the process.  This
+#!/usr/bin/env python
+"""Builds graphs from the lines in a gziped bro log in a temporary location,
+echos out the number of records incorporated into graphs in the process.  This
 script is really only useful as a sanatiy check to see how we'll we're doing,
 and making sure that all expected files are being brough into a graph.
 
@@ -14,8 +14,9 @@ sys.path.append(os.path.join('..'))
 
 import argparse
 import brotools.graphs
+import gzip
 
 for path in sys.stdin:
     count = 0
-    with open(path.strip(), 'r') as h:
+    with gzip.open(path.strip(), 'rb') as h:
         print sum([len(g) for g in brotools.graphs.graphs(h)])
