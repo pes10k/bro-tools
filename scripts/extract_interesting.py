10b8fb2ba125c4d19f94cf3366b62aac346e5d06
Peter Snyder <snyderp@gmail.com>
2014-10-28 17:42:21 -0500

    add new extract tool

---
+++ scripts/extract_interesting.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Receive a large number of graphs, in pickle files, and write back only
+those graphs that include domains that we care about."""
+
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+import os
+import brotools.reports
+import brotools.records
+import datetime
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+parser = brotools.reports.marketing_cli_parser(sys.modules[__name__].__doc__)
+count, ins, out, debug, marketers, args = brotools.reports.parse_marketing_cli_args(parser)
+
+for path, graphs in ins():
+
+    index += 1
+    debug("{0}. Considering {1}".format(index, path))
+    debug("{0}. Found {1} graphs".format(index, len(graphs)))
+    for g in graphs:
+        for marketer in marketers:
+            if len(marketer.nodes_for_domains(g)) == 0:
+                continue
+            debug("{0}. Found graph for {1}".format(index, marketer.name()))
+            dst_path = path + ".important"
+            with open(dst_path, 'a') as h:
+                pickle.dump(graph, h)
+            break
