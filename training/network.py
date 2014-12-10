7dd5abad980e2e62917141a4163bd95f5f178667
Peter Snyder <snyderp@gmail.com>
2014-12-10 08:38:01 -0600

    add new command line config options

--- training/network.py
+++ training/network.py
@@ -15,9 +15,9 @@ parser.add_argument('--trains', '-t', type=int, default=1,
                          "over the data set aside as training data before " +
                          "evaluating the network on the set aside data.")
 parser.add_argument('--normalize', '-n', action="store_true")
-parser.add_argument('--wrong', '-w', type=float, default=(.05 / 20.0),
+parser.add_argument('--wrong', '-w', type=float, default=0.0,
                     help="The amount to punish incorrect misses by.")
-parser.add_argument('--correct', '-c', type=float, default=(.05 / 20.0),
+parser.add_argument('--correct', '-c', type=float, default=0.0,
                     help="The amount to punish correct misses by.")
 parser.add_argument('--reward', '-r', type=float, default=(.05 / 20),
                     help="The amount to reward correct predictions.")
@@ -28,12 +28,18 @@ parser.add_argument('--rec', type=int, default=0,
                     help="If provided, the maximum number of records " +
                          "to evaluate.  This is really only useful " +
                          "to inspect and debug the network.")
+parser.add_argument('--outs', '-o', default=3, type=int,
+                    help="The number of output neurons to use.  Only makes " +
+                         "sense to use 2 or 3.")
 parser.add_argument("--debug", "-d", action="store_true")
 args = parser.parse_args()
 
 def debug(msg):
     if args.debug:
-        pprint(msg)
+        if isinstance(msg, basestring):
+            print msg
+        else:
+            pprint(msg)
 
 # Params to tune
 reward = args.reward
@@ -43,7 +49,7 @@ max_weight = args.max
 normalize = args.normalize
 training_rounds = args.trains
 random_assignment = False
-num_output_neurons = 3
+num_output_neurons = args.outs
 
 features = (
     ("time_from_referrer", soms.TimeFromReferrerSOM()),
@@ -134,7 +140,7 @@ def value_for_feature(label, row):
         return None
     return value
 
-testing_indexes = []
+testing_indexes = set()
 training_row_index = -1
 for round in range(training_rounds):
     record_count = 0
@@ -168,7 +174,7 @@ for round in range(training_rounds):
                 is_testing = training_row_index % 3 == 0
         
             if is_testing:
-                testing_indexes.append(training_row_index)
+                testing_indexes.add(training_row_index)
                 debug("Moving record to evaluation set.")
                 continue
    
@@ -216,6 +222,7 @@ for round in range(training_rounds):
             # outside the allowed bounds.
             current_weight = weights[subword_index][winning_neuron_index][label_index]
             new_weight = adjust_weight(current_weight + reward)
+            weights[subword_index][winning_neuron_index][label_index] = new_weight
             normalizing_counts[subword_index][label_index] += 1
 
             # Now apply punishments to other neurons in the same subword /
@@ -238,30 +245,41 @@ for round in range(training_rounds):
                     punished_weight = adjust_weight(current_weight - adjustment)
                     weights[subword_index][s][v] = punished_weight
 
+# If we're in debug mode, print out the "learned" weights before normalization.
+debug("")
+debug("Weights pre-normalization")
+debug("---")
+debug(weights)
+
 if normalize:
-    pprint(weights[0][0])
     for input_index in range(len(weights)):
         for neuron_index in range(len(weights[input_index])):
             for j in range(len(weights[input_index][neuron_index])):
                 if normalizing_counts[input_index][j] != 0:
                     weights[input_index][neuron_index][j] /= normalizing_counts[input_index][j]
-    pprint(weights[0][0])
+    debug("")
+    debug("Weights post normalization")
+    debug("---")
+    debug(weights)
 
-testing_row_index = -1
 nn_predictions = []
 algo_predictions = []
 nothing_predicitons = []
 correct_rs = []
 
 results = numpy.zeros((3, 2))
+testing_row_index = -1
 for row in sqltypes.raw_records():
     testing_row_index += 1
 
     # If the current row was not set aside to be included in the training set,
-    # don't consider it
+    # don't consider it.  `testing_indexes` is a set of the indexes that 
+    # were selected previously as being for evaluation and not for training.
     if testing_row_index not in testing_indexes:
         continue
-    
+  
+    # Now keep track of which output neuron each input neuron votes for,
+    # with a simple counter for each output
     index_votes = numpy.zeros(num_output_neurons)
     correct = desired_index(row)
     correct_rs.append(correct)
@@ -277,6 +295,7 @@ for row in sqltypes.raw_records():
         for j in range(len(index_votes)):
             index_votes[j] += weights[i][winning_index][j]
 
+    debug(index_votes)
     winning_index = list(index_votes).index(max(index_votes))
 
     # correct for some minor encoding errors
@@ -289,7 +308,6 @@ for row in sqltypes.raw_records():
 #     print "Predict: {0}".format(winning_index)
 # 
     results[0][0 if winning_index == correct else 1] += 1
-
 
     nn_predictions.append(abs(winning_index - correct))
 
@@ -308,9 +326,6 @@ for row in sqltypes.raw_records():
     results[2][0 if 0 == correct else 1] += 1
     nothing_predicitons.append(abs(0 - correct))
 
-print algo_predictions
-
 print "Algo:     {0}".format(1 - sum(algo_predictions) / float(len(algo_predictions)))
 print "NN:       {0}".format(1 - (sum(nn_predictions) / float(len(nn_predictions))))
 print "No Guess: {0}".format(1 - sum(nothing_predicitons) / float(len(nothing_predicitons)))
-pprint(results)
