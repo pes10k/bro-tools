6b61359356a882102317aa5c3f8724161fc9f0f7
Peter Snyder <snyderp@gmail.com>
2015-02-11 18:51:00 -0500

    change-os to extract_steals

--- scripts/extract_steals.py
+++ scripts/extract_steals.py
@@ -8,7 +8,7 @@ import os.path
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 import brotools.reports
-from stuffing.affiliate import STUFF, SET
+from stuffing.affiliate import STUFF, SET, CART
 
 try:
     import cPickle as pickle
@@ -38,49 +38,77 @@ count, ins, out, debug, marketers, args = cli_params
 # },
 history_by_client = {}
 index = 0
+old_path = None
 debug("Preparing to start reading {0} pickled data".format(count))
-for path, graphs in ins():
-    index += 1
-    debug("{0}-{1}. Considering {2}".format(index, count, path))
-    debug("{0}-{1}. Found {2} graphs".format(index, count, len(graphs)))
-    for g in graphs:
-        for marketer in marketers:
+marketer_lookup = {}
 
-            # See if we can find a session tracking cookie for this visitor
-            # in this graph.  If not, then we know there are no cookie stuffs,
-            # checkouts, or other relevant activity in the graph we
-            # care about, so we can continue
-            hash_key = marketer.session_id_for_graph(g)
-            if not hash_key:
-                continue
+for path, g in ins():
+    if not old_path or old_path != path:
+        index += 1
+        old_path = path
+        debug("{0}-{1}. Considering {2}".format(index, count, path))
+    for marketer in marketers:
 
-            # First extract the dict for this marketer
-            try:
-                client_dict = history_by_client[marketer.name()]
-            except:
-                client_dict = {}
-                history_by_client[marketer.name()] = client_dict
+        if marketer.name() not in marketer_lookup:
+          marketer_lookup[marketer.name()] = marketer
 
-            # Next, try to extract a history object for this client
-            # out of the dict of clients for the given marketer
-            try:
-                history = client_dict[hash_key]
-            except KeyError:
-                history = marketer(g)
-                client_dict[hash_key] = history
+        # See if we can find a session tracking cookie for this visitor
+        # in this graph.  If not, then we know there are no cookie stuffs,
+        # checkouts, or other relevant activity in the graph we
+        # care about, so we can continue
+        hash_key = marketer.session_id_for_graph(g)
+        if not hash_key:
+            continue
+
+        # First extract the dict for this marketer
+        try:
+            client_dict = history_by_client[marketer.name()]
+        except:
+            client_dict = {}
+            history_by_client[marketer.name()] = client_dict
+
+        # Next, try to extract a history object for this client
+        # out of the dict of clients for the given marketer
+        try:
+            history = client_dict[hash_key]
+            history.consider(g)
+        except KeyError:
+            history = marketer(g)
+            client_dict[hash_key] = history
+
+checkout_count = 0
+stuffs_count = 0
+sets_count = 0
+parties_count = 0
+steals_count = 0
+conservative_stuffs_count = 0
 
 for marketer_name, histories in history_by_client.items():
+    marketer = marketer_lookup[marketer_name]
     for client_hash, h in histories.items():
         for c in h.checkouts(seconds=args.secs, cookie_ttl=args.ttl):
+            checkout_count += 1
             set_indexes = []
             stuff_indexes = []
+            tags_in_checkout = set() 
+            stuff_tags_in_checkout = set()
             for i, (r, g, t) in enumerate(c.cookie_history()):
+                if t == CART:
+                    continue
+
                 if t == STUFF:
+                    stuffs_count += 1
                     stuff_indexes.append(i)
+                    stuff_tags_in_checkout.add(marketer.get_referrer_tag(r))
                 elif t == SET:
+                    sets_count += 1
                     set_indexes.append(i)
+                conservative_stuffs_count += len(stuff_tags_in_checkout)
+                tags_in_checkout.add(marketer.get_referrer_tag(r))
+            parties_count += len(tags_in_checkout)
             if (len(set_indexes) > 0 and len(stuff_indexes) > 0 and
-               stuff_indexes[-1] > set_indexes[-1]):
-                debug("Found a steal")
-                debug(str(c))
-                pickle.dump(c, out)
+                stuff_indexes[-1] > set_indexes[-1]):
+                steals_count += 1
+
+out.write("Checkouts: {}\nStuffs: {}\nSets: {}\nSteals: {}\n".format(checkout_count, stuffs_count, sets_count, steals_count))
+out.write("Parties: {}\n".format(parties_count))
