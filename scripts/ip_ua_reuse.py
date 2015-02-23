9866f2ad271a5e0005aecab04c39d6b24feecba0
Peter Snyder <snyderp@gmail.com>
2015-02-22 18:51:26 -0600

    -

--- scripts/ip_ua_reuse.py
+++ scripts/ip_ua_reuse.py
@@ -35,28 +35,26 @@ index = 0
 ip_tokens = {}
 ip_ua_tokens = {}
 
-for path, graphs in ins():
+for path, g in ins():
     index += 1
     debug("{0}-{1}. Considering {2}".format(index, count, path))
-    debug("{0}-{1}. Found {2} graphs".format(index, count, len(graphs)))
-    for g in graphs:
-        nodes = g.nodes_for_hosts("www.amazon.com", "amazon.com")
-        for n in nodes:
-            token = stuffing.amazon.session_token(n)
-            if not token:
-                continue
-            key = g.ip + " " + n.user_agent
-            if g.ip not in ip_tokens:
-                ip_tokens[g.ip] = {}
-            if token not in ip_tokens[g.ip]:
-                ip_tokens[g.ip][token] = []
-            ip_tokens[g.ip][token].append(n.ts)
+    nodes = g.nodes_for_hosts("www.amazon.com", "amazon.com")
+    for n in nodes:
+        token = stuffing.amazon.session_token(n)
+        if not token:
+            continue
+        key = g.ip + " " + n.user_agent
+        if g.ip not in ip_tokens:
+            ip_tokens[g.ip] = {}
+        if token not in ip_tokens[g.ip]:
+            ip_tokens[g.ip][token] = []
+        ip_tokens[g.ip][token].append(n.ts)
 
-            if key not in ip_ua_tokens:
-                ip_ua_tokens[key] = {}
-            if token not in ip_ua_tokens[key]:
-                ip_ua_tokens[key][token] = []
-            ip_ua_tokens[key][token].append(n.ts)
+        if key not in ip_ua_tokens:
+            ip_ua_tokens[key] = {}
+        if token not in ip_ua_tokens[key]:
+            ip_ua_tokens[key][token] = []
+        ip_ua_tokens[key][token].append(n.ts)
 
 num_reused_ips = sum([1 if len(v) > 1 else 0 for v in ip_tokens.values()])
 num_collisions = sum([1 if collision(tokens.values()) else 0 for tokens in [t for t in ip_tokens.values()]])
